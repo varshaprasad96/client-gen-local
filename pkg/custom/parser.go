82e90c0cd92ba6ee48565d20a1c95c810e9796c4
varshaprasad96 <varshaprasad96@gmail.com>
2022-04-06 23:30:59 -0700

    custom generator

--- pkg/custom/parser.go
+++ pkg/custom/parser.go
@@ -2,13 +2,17 @@ package custom
 
 import (
 	"fmt"
+	"go/types"
+	"io"
+	"strings"
 
+	"sigs.k8s.io/controller-tools/pkg/loader"
 	"sigs.k8s.io/controller-tools/pkg/markers"
 )
 
 var (
 	// RuleDefinition is a marker for defining rules
-	RuleDefinition = markers.Must(markers.MakeDefinition("custom:generate", markers.DescribesPackage, Rule{}))
+	RuleDefinition = markers.Must(markers.MakeDefinition("genclient", markers.DescribesType, Rule{}))
 )
 
 type Rule struct {
@@ -24,7 +28,7 @@ type Rule struct {
 	// is not known at authorization time.
 	ResourceNames []string `marker:",optional"`
 	// Verbs specifies the (lowercase) kubernetes API verbs that this rule encompasses.
-	Verbs []string
+	Verbs []string `marker:",optional"`
 	// URL specifies the non-resource URLs that this rule encompasses.
 	URLs []string `marker:"urls,optional"`
 	// Namespace specifies the scope of the Rule.
@@ -45,3 +49,66 @@ type ruleKey struct {
 func (key ruleKey) String() string {
 	return fmt.Sprintf("%s + %s + %s + %s + %s", key.Types, key.Groups, key.Resources, key.ResourceNames, key.URLs)
 }
+
+type codeWriter struct {
+	out io.Writer
+}
+
+// Line writes a single line.
+func (c *codeWriter) Line(line string) {
+	fmt.Fprintln(c.out, line)
+}
+
+type configMethodWriter struct {
+	*importsList
+	pkg loader.Package
+	*codeWriter
+}
+
+func (c *configMethodWriter) GenerateConfigMethod(root *loader.Package, info *markers.TypeInfo) {
+	typeInfo := root.TypesInfo.TypeOf(info.RawSpec.Name)
+	if typeInfo == types.Typ[types.Invalid] {
+		root.AddError(loader.ErrFromNode(fmt.Errorf("unknown type: %s", info.Name), info.RawSpec))
+	}
+
+	if strings.HasSuffix(typeInfo.String(), "Status") || strings.HasSuffix(typeInfo.String(), "Spec") {
+		return
+	}
+
+	c.Line("// DONOT EDIT!!")
+	c.Line(newClientsetForConfigTemplate)
+
+	// Add the imports
+	importsList := []string{"k8s.io/client-go/kubernetes", "github.com/kcp-dev/kcp-client-wrappers/kcp", "k8s.io/client-go/rest"}
+	for _, imp := range importsList {
+		c.NeedImport(imp)
+	}
+
+}
+
+var newClientsetForConfigTemplate = `
+// NewForConfig creates a new Clientset for the given config.
+// If config's RateLimiter is not set and QPS and Burst are acceptable, 
+// NewForConfig will generate a rate-limiter in configShallowCopy.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
+func NewForConfig(c *rest.Config) (*ClusterClient, error) {
+	client, err := rest.HTTPClientFor(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating HTTP client: %w", err)
+	}
+
+	clusterRoundTripper := kcp.NewClusterRoundTripper(client.Transport)
+	client.Transport = clusterRoundTripper
+
+	delegate, err := kubernetes.NewForConfigAndClient(config, client)
+	if err != nil {
+		return nil, fmt.Errorf("error creating delegate clientset: %w", err)
+	}
+
+	return &ClusterClient{
+		delegate: delegate,
+	}, nil
+
+}
+`
--- testdata/controller.go
+++ testdata/controller.go
@@ -1,5 +1,27 @@
 package controller
 
-// +custom:generate:types=memcached,groups=batch.io,resources=cronjobs,verbs=get;watch;create
-// +custom:generate:types=something,groups=batch.io,resources=cronjobs/status,verbs=get;update;patch
-// +custom:generate:types=test,groups=art,resources=jobs,verbs=get
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+type Foo struct {
+	// TypeMeta comments should NOT appear in the CRD spec
+	metav1.TypeMeta `json:",inline"`
+	// ObjectMeta comments should NOT appear in the CRD spec
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec comments SHOULD appear in the CRD spec
+	Spec FooSpec `json:"spec,omitempty"`
+	// Status comments SHOULD appear in the CRD spec
+	Status FooStatus `json:"status,omitempty"`
+}
+
+type FooStatus struct{}
+
+type FooSpec struct {
+	// This tests that defaulted fields are stripped for v1beta1,
+	// but not for v1
+	// +kubebuilder:default=fooDefaultString
+	DefaultedString string `json:"defaultedString"`
+}
